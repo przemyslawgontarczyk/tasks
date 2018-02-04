38fcb0c9a3b5b9a33e449b5e2d2d852c1dcbe493
“przemyslawgontarczyk <przemyslawgontarczyk@gmail.com>
2018-02-04 22:16:33 +0100

    15.3

--- src/main/java/com/crud/tasks/domain/Task.java
+++ src/main/java/com/crud/tasks/domain/Task.java
@@ -3,7 +3,6 @@ package com.crud.tasks.domain;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.boot.autoconfigure.web.ResourceProperties;
 
 import javax.persistence.*;
 
